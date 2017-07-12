8cf2eabec01b51504351471623b88c565b63d3f4
pavlvoic265 <pavlvoic265@gmail.com>
2017-07-12 07:14:31 +0200

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">React Title App</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="#/" target="_blank">Marko Pavlovic</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+module.exports = Nav;
