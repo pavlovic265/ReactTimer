f1368d0e85b2f6e13e9c06772fce509649049d95
pavlvoic265 <pavlvoic265@gmail.com>
2017-07-13 08:05:11 +0200

    Clock test and clock style

---
+++ app/tests/components/Clock.test.jsx
@@ -0,0 +1,43 @@
+var React = require('React');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Clock = require('Clock');
+
+describe('Clock', () => {
+    it('should exist', () => {
+        expect(Clock).toExist();
+    });
+
+    describe('render', () => {
+        it('should render clock to outout', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+            var expected = '01:02';
+
+            expect(actualText).toBe(expected);
+        });
+    });
+
+    describe('formatSeconds', () => {
+        it('should foramt seconds', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds); 
+            expect(actual).toBe(expected);
+        });
+        
+        it('should foramt seconds when min/sec less then 10', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 61;
+            var expected = '01:01';
+            var actual = clock.formatSeconds(seconds); 
+            expect(actual).toBe(expected);
+        });
+
+    });
+});
